e6dc5b7d47d6c62b2c78e253226104ccf44fb0be
NakulMishra <nakulmi007@gmail.com>
2025-10-01 13:01:06 +0530

    Add a task in Array in script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const todoInput = document.getElementById("todo-input");
+    const addTaskButton = document.getElementById("add-task-btn");
+    const todoList = document.getElementById("todo-list");
+
+    let tasks = [];
+
+    addTaskButton.addEventListener("click", addTask);
+    
+    todoInput.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            addTask();
+        }
+    });
+
+    function addTask() {
+        const taskText = todoInput.value.trim();
+        if(taskText === "")return;
+
+        const newTask = {
+            id : Date.now(),   
+            text : taskText,
+            completed : false
+        }
+
+        tasks.push(newTask);   
+        todoInput.value = "";  
+    }
+})
