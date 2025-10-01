c1f82aab3da3948d80e4b23cfb4ae156bacbb7f9
NakulMishra <nakulmi007@gmail.com>
2025-10-01 13:13:40 +0530

    Handling local storage and render tasks.

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const addTaskButton = document.getElementById("add-task-btn");
     const todoList = document.getElementById("todo-list");
 
-    let tasks = [];
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];   // To store the task and it's information. if their is nothing to grab in local strorage nothing to parse, then we use the empty array.
+    // JSON.parse is used to convert string to it's original data structure. Here string->array
+
+    tasks.forEach((task) => renderTask(task));
 
     addTaskButton.addEventListener("click", addTask);
     
@@ -23,7 +26,18 @@ document.addEventListener("DOMContentLoaded", () => {
             completed : false
         }
 
-        tasks.push(newTask);   
+        tasks.push(newTask); 
+        saveTasks();  
         todoInput.value = "";  
     }
+
+    function renderTask(task) {
+        console.log(task);
+    }
+
+    // Function to store array in local storage 
+    function saveTasks() {
+        localStorage.setItem("tasks", JSON.stringify(tasks));  // it takes key:value pair and both has to be string. JSON.stringify is used to convert original data structure to string. Here array->string
+    }
+
 })
